390100c64ef26725f0355ab9f8645dc7874a798f
Dejan Tomic <hi@iamdejan.com>
2026-07-19 16:51:50 +0200

    tasks: tab switching through a real handler, segmented tone

--- front/items/elements/task/task.js
+++ front/items/elements/task/task.js
@@ -73,6 +73,11 @@ onetype.AddonReady('elements', (elements) =>
 				{ id: 'comments', label: 'Comments', icon: 'forum', count: this.task.comments.length }
 			];
 
+			this.switch = () => ({ value }) =>
+			{
+				this.tab = value;
+			};
+
 			this.change = (task) =>
 			{
 				this.task = task;
@@ -182,7 +187,7 @@ onetype.AddonReady('elements', (elements) =>
 					</div>
 
 					<div class="tabs">
-						<e-navigation-tabs :items="tabs()" :active="tab" :_change="({ value }) => tab = value"></e-navigation-tabs>
+						<e-navigation-tabs :items="tabs()" :active="tab" tone="segmented" :stretch="true" :background="1" :_change="switch()"></e-navigation-tabs>
 					</div>
 
 					<div ot-if="tab === 'overview'" class="body">
